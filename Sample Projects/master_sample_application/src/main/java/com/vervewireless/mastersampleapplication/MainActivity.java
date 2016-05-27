0ab13bb030077563f7ff0fd2b7abfe327413107a
Christina Long <clong714@gmail.com>
2016-05-27 13:03:44 -0400

    Add v3.1.1

---
+++ Sample Projects/master_sample_application/src/main/java/com/vervewireless/mastersampleapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.vervewireless.mastersampleapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.vervewireless.advert.VerveAdSDK;
+import com.vervewireless.mastersampleapplication.dfp.DfpBannerSample;
+import com.vervewireless.mastersampleapplication.dfp.DfpInterstitialSample;
+import com.vervewireless.mastersampleapplication.mopub.MoPubBannerSample;
+import com.vervewireless.mastersampleapplication.mopub.MoPubInterstitialSample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(getTitle() + " - " + VerveAdSDK.sdkVersion());
+        init();
+    }
+
+    private void init() {
+        ListView samples = (ListView) findViewById(R.id.listSamples);
+        List<SampleItem> items = new ArrayList<>();
+        // Verve
+        items.add(new SampleItem(getString(R.string.verve)));
+        items.add(new SampleItem(VerveSample.class, getString(R.string.verve_sdk_samples)));
+        items.add(new SampleItem(SplashManagerSample.class, getString(R.string.splash_manager_sample)));
+        // Dfp
+        items.add(new SampleItem(getString(R.string.dfp)));
+        items.add(new SampleItem(DfpBannerSample.class, getString(R.string.dfp_banner_sample)));
+        items.add(new SampleItem(DfpInterstitialSample.class, getString(R.string.dfp_interstitial_sample)));
+        // MoPub
+        items.add(new SampleItem(getString(R.string.mopub)));
+        items.add(new SampleItem(MoPubBannerSample.class, getString(R.string.mopub_banner_sample)));
+        items.add(new SampleItem(MoPubInterstitialSample.class, getString(R.string.mopub_interstitial_sample)));
+        final SamplesAdapter adapter = new SamplesAdapter(items);
+        samples.setAdapter(adapter);
+        samples.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Class target = adapter.getItem(position).getTarget();
+                if (target != null) {
+                    Intent intent = new Intent(MainActivity.this, target);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
---
+++ Sample Projects/master_sample_application/src/main/java/com/vervewireless/mastersampleapplication/SampleApplication.java
@@ -0,0 +1,72 @@
+package com.vervewireless.mastersampleapplication;
+
+import android.app.Application;
+
+import com.vervewireless.advert.Category;
+import com.vervewireless.advert.LocationPermissionDelegate;
+import com.vervewireless.advert.SplashAdManager;
+import com.vervewireless.advert.StoragePermissionDelegate;
+import com.vervewireless.advert.VerveAdSDK;
+
+public class SampleApplication extends Application {
+
+    private static final String MY_AD_KEYWORD = "adsdkexample";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initVerveAdSDK();
+    }
+
+    private void initVerveAdSDK() {
+        VerveAdSDK.initialize(this, new VerveAdSDK.InitializationListener() {
+            @Override
+            public void onInitialized(VerveAdSDK verveAdSDK) {
+                // you can modify the VerveAdSDK object after the initialization had finished
+                // e.g. configure permission delegates or Splash Manager...
+                SplashAdManager splashAdManager = SplashAdManager.instance();
+                splashAdManager.setAdKeyword(MY_AD_KEYWORD);
+                splashAdManager.setCategory(Category.HOME_PAGE);
+                // Show provided Splash Image on app startup
+                /*
+                * Images must be named correctly
+                *
+                * verve_splash - phone portrait
+                * verve_splash_land - phone landscape
+                * verve_tablet_splash - tablet portrait
+                * verve_tablet_splash_land - tablet landscape
+                *
+                */
+                splashAdManager.setShowSplashImageOnAppStart(true);
+                splashAdManager.start();
+
+                // Only used when targeting API 23 and up
+                verveAdSDK.setLocationPermissionDelegate(new LocationPermissionDelegate() {
+                    @Override
+                    public boolean shouldAdLibraryRequestLocationPermission() {
+                        /*
+                        * The library will request user permissions to use location services when the app is brought to foreground.
+                        * If you want to manually request permission to use location services or you want to delay it until some
+                        * later time, you can cancel library's request.
+                        * */
+                        return true;
+                    }
+                });
+
+                // Only used when targeting API 23 and up
+                verveAdSDK.setStoragePermissionDelegate(new StoragePermissionDelegate() {
+                    @Override
+                    public boolean shouldAdLibraryRequestStoragePermission() {
+                         /*
+                        * The library will request user permissions to use external storage when needed.
+                        * If you want to manually request permission to use external storage or you want to delay it until some
+                        * later time, you can cancel library's request.
+                        * */
+                        return true;
+                    }
+                });
+            }
+        });
+
+    }
+}
